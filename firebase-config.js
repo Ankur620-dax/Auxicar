78b0c6994f43fb02314f39dcba9bfd1d02528344
Ankur620-dax <daxter3006@gmail.com>
2025-07-07 10:24:04 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ83uo3N5W7QQdlkb6Dzr8liU1oICU53Q",
+  authDomain: "auxicar-2aa1a.firebaseapp.com",
+  projectId: "auxicar-2aa1a",
+  storageBucket: "auxicar-2aa1a.firebasestorage.app",
+  messagingSenderId: "913467701523",
+  appId: "1:913467701523:web:7f3e27235a9e728c3b02e6",
+  measurementId: "G-7X90RNVJ1P"
+};
+
